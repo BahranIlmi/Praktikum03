3398bc6d78babd05a04b161313bcac10d0486d38
BahranIlmi <bahranilmi13@gmail.com>
2017-11-07 18:17:05 +0800

    Menghitung Selisih Absolute X dan Y

--- Praktek0301.cpp
+++ Praktek0301.cpp
@@ -8,5 +8,14 @@ int main(){
 	cout<<"Masukan Nilai X : ";
 	cin>>x;
 	cout<<"Masukan Nilai Y : ";
-	cin>>y;	
+	cin>>y;
+		
+	if (x>y){
+		z=x-y;
+	}
+	else if (y>x){
+		z=y-x;
+	}
+	cout<<"Hasil : "<<z;
+		
 }
