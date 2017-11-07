48985161d1bfbdaa0923022643f64a261085d13c
BahranIlmi <bahranilmi13@gmail.com>
2017-11-07 18:30:01 +0800

    Input Nilai

--- Praktek0303.cpp
+++ Praktek0303.cpp
@@ -3,5 +3,11 @@ using namespace std;
 
 int main(){
 	int gp,jk;
-	int lembur,gt,pajak,gk;		
+	int lembur,gt,pajak,gk;
+	
+	cout<<"Perhitungan Gaji Karyawan"<<endl;
+	cout<<"Masukan Gaji Pokok Anda : Rp. ";
+	cin >>gp;
+	cout <<"Masukan Jam Kerja Anda : ";
+	cin >>jk;	
 }
