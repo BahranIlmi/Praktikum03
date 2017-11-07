d6f468d93836c0bacd91b92bef5fb3a12bc664f1
BahranIlmi <bahranilmi13@gmail.com>
2017-11-07 18:22:03 +0800

    Input Pilihan

--- Praktek0302.cpp
+++ Praktek0302.cpp
@@ -3,4 +3,10 @@ using namespace std;
 
 int main(int argc, char** argv){
 	int panjang, lebar, luas, pilihan, alas, tinggi;
+	
+	cout <<"1.Persegi Panjang"<<endl;
+	cout <<"2.Segitiga"<<endl;
+	cout <<"Masukan Pilihan = ";
+	cin >>pilihan;
+	
 }
