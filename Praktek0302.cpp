162f7c9446cb1a3f2d4f0f9c2ff998afddf05746
BahranIlmi <bahranilmi13@gmail.com>
2017-11-07 18:26:10 +0800

    Menghitung Luas Persegi Panjang atau Segitiga

--- Praktek0302.cpp
+++ Praktek0302.cpp
@@ -9,4 +9,23 @@ int main(int argc, char** argv){
 	cout <<"Masukan Pilihan = ";
 	cin >>pilihan;
 	
+		switch(pilihan){
+		case 1:
+		cout<<"Masukan Panjang = ";
+		cin>>panjang;
+		cout<<"Masukan Lebar = ";
+		cin>>lebar;
+		luas =panjang * lebar;
+		cout<<"Luas Persegi Panjang="<<luas<<endl;
+		system("pause");
+		break;
+		case 2:
+		cout<<"Masukan Panjang Alas = ";
+		cin>>alas;
+		cout<<"Masukan Tinggi = ";
+		cin>>tinggi;
+		luas=0.5*alas*tinggi;
+		cout<<"Luas Segitiga="<<luas<<endl;
+		system("pause");
+	}	
 }
